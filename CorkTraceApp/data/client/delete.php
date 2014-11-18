2754545e5026859839d8685d9cef9c2ba6d3be21
cedricdelhaes <delhaes.c@gmail.com>
2014-11-18 17:24:39 +0100

    fix comm ihm crud

--- CorkTraceApp/data/client/delete.php
+++ CorkTraceApp/data/client/delete.php
@@ -2,8 +2,10 @@
 
 	require_once("../orm/Client.php");
 
+	$data = json_decode($_POST['data']);
+
 	//Vérif des données entrante
-	$cli_id	= isset ($_POST['cli_id']) ? $_POST['cli_id'] : "undefined";
+	$cli_id		= isset ($data->{'cli_id'}) ? $data->{'cli_id'} : "undefined";
 
 	/*$cli_id	= isset ($_GET['cli_id']) ? $_GET['cli_id'] : "undefined";*/
 
