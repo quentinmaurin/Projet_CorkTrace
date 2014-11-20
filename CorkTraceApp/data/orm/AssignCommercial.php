c73d7af8413986710d54ddd0f1f799f8dbfb1f42
cedricdelhaes <delhaes.c@gmail.com>
2014-11-20 14:15:50 +0100

    ajout commande client final, debug load list

--- CorkTraceApp/data/orm/AssignCommercial.php
+++ CorkTraceApp/data/orm/AssignCommercial.php
@@ -14,6 +14,16 @@ final class AssignCommercial extends Table{
             "COM_ID" => "COM_ID"
         );
 	}
+    public function getCommercialByClient($cli_id){
+
+        $query = "SELECT t_clicom_clc.clc_id, t_clicom_clc.cli_id, t_clicom_clc.com_id, t_commercial_com.com_nom
+        FROM t_clicom_clc
+        INNER JOIN t_commercial_com ON t_clicom_clc.com_id=t_commercial_com.com_id
+        WHERE t_clicom_clc.cli_id=".$cli_id;
+        $allRows = $this->db->getResponse($query);
+        
+        return $allRows;
+    }
 }
 
 ?>
