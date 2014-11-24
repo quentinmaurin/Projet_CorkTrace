bb3766759bc7927364c2dcaf0df1afc6cc83b91f
loictrichaud <loic.trichaud@imerir.com>
2014-11-24 14:31:20 +0100

    Ajout page connexion

---
+++ index.php
@@ -0,0 +1,36 @@
+﻿<html>
+
+	<head>
+		<link rel="stylesheet" type="text/css" media="all" href="CorkTraceApp/css/bootstrap.css" >
+		<link rel="stylesheet" type="text/css" media="all" href="CorkTraceApp/css/style.css" >
+		<title>CorkTrace App</title>
+	</head>
+
+	<body>
+		<div class="container" style="padding-top:100px; width:400px;"> 
+			<div style="margin-auto:0 auto; text-align:center;">
+				<img src="CorkTraceApp/img/logo3.png" style="width:100%;">
+			</div>
+				
+			<div id="login_contenu">
+				<div class="row-fluid">
+					<div class="span12 well">
+						<center>
+							<img src="CorkTraceApp/img/admin.png" class="img-circle">
+							<h4>Administrateur</h4>
+						</center>
+						<br />
+						<a href="CorkTraceApp/" class="btn btn-marron  btn-large btn-block">Connexion</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+
+	<footer>
+		<center>
+			<p>&copy; CorkTrace 2014 - Tous droits réservés.</p>
+		</center>
+	</footer>
+	
+</html>
